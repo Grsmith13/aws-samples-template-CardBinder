52af643920400a98f642ffcdc8b370ffa7f5f2ee
grsmith44 <grsmith44@tntech.edu>
2024-12-07 18:05:44 -0600

    added API functionality

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,16 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 Amplify.configure(outputs);
 
+const existingConfig = Amplify.getConfig();
+
+Amplify.configure({
+  ...existingConfig,
+  API: {
+    ...existingConfig.API,
+    REST: outputs.custom.API,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator>
