318e0a8855d1bf804929e216e00c6c368b051330
grsmith44 <grsmith44@tntech.edu>
2024-12-07 18:16:24 -0600

    reverted changes further

--- src/main.tsx
+++ src/main.tsx
@@ -8,16 +8,6 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 Amplify.configure(outputs);
 
-const existingConfig = Amplify.getConfig();
-
-Amplify.configure({
-  ...existingConfig,
-  API: {
-    ...existingConfig.API,
-    REST: outputs.custom.API,
-  },
-});
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator>
